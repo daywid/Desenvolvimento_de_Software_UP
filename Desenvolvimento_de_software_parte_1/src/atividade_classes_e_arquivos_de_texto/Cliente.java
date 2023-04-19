9608f4cb3815f2e1a4a6e0a90466d0451ed5c31a
Daywid <88506180+daywid@users.noreply.github.com>
2023-04-19 12:50:13 -0300

    Update Cliente.java

--- Desenvolvimento_de_software_parte_1/src/atividade_classes_e_arquivos_de_texto/Cliente.java
+++ Desenvolvimento_de_software_parte_1/src/atividade_classes_e_arquivos_de_texto/Cliente.java
@@ -1,21 +1,15 @@
-package atividade_classes_e_arquivos_de_texto;
 /*
 Desenvolva o que se pede
-
 1. Implemente uma classe cliente com os atributos {cpf, nome, email e telefone}
-
 1.1 A classe cliente deve possuir um método salvar,
 que grava os dados dos atributos em um arquivo clientes.csv, 
 que deve possuir um cabeçalho e ser separado por vírgula
-
 1.2 A classe deve possuir um método find_one() que recebe o cpf, 
 abre o arquivo clientes.csv, localiza o cpf, 
 carrega os dados relativos a esse cpf nos atributos do objeto
-
 1.3 A classe deve possuir un método find_all() 
 que abre o arquivo clientes.csv e retorna uma lista de objetos 
 com os dados gravados.
-
 1.4 Sempre que um dos métodos de busca (find_one e find_all) não encontrarem, 
 deve gerar um exceção (pesquise pelo comando throw ou throws  do java).
 */
@@ -29,84 +23,78 @@ import java.util.List;
 
 public class Cliente {
 
-	public String cpf = "";
-	public String nome = "";
-	public String email = "";
-	public String telefone = "" ;
-	
-	public void Salvar() {
-		String separador = ",";
-		try {
-			FileWriter arquivo = new FileWriter("C:\\Users\\Aluno\\Documents\\day\\clientes.csv",true);
-			String linha = this.cpf+ separador +
-					this.nome+ separador + 
-					this.email;
-			arquivo.write(linha + "\n");
-			arquivo.flush();
-			arquivo.close();	
-		
-		} catch (IOException e) {
-			e.printStackTrace();}
-		}
-		
-	public void find_one(String cpf) throws IOException {
-	    FileReader arquivo = null;
-	    BufferedReader leitor = null;
-	    
-	    try {
-	        arquivo = new FileReader("clientes.csv");
-	        leitor = new BufferedReader(arquivo);
-	        
-	        String linha = leitor.readLine();
-	        while (linha != null) {
-	            String[] dados = linha.split(",");
-	            if (dados[0].equals(cpf)) {
-	                this.cpf = dados[0];
-	                this.nome = dados[1];
-	                this.email = dados[2];
-	                this.telefone = dados[3];
-	                return;
-	            }
+    private String cpf;
+    private String nome;
+    private String email;
+    private String telefone;
 
-	            linha = leitor.readLine();
-	        }
-	        
-	        throw new IllegalArgumentException("Cliente não encontrado");
-	        
-	    } finally {
-	        if (leitor != null) {
-	            leitor.close();
-	        }
-	        
-	        if (arquivo != null) {
-	            arquivo.close();
-	        }
-	    }
-	}
-	public List<Cliente> find_all() throws IOException{
-		FileReader arquivo = new FileReader("clientes.csv");
-		BufferedReader leitor = new BufferedReader(arquivo);
-		
-		List<Cliente> clientes = new ArrayList<Cliente>();
-		String linha = leitor.readLine();
-		while (linha != null) {
-			String[] dados = linha.split(",");
-			Cliente cliente = new Cliente();
-			cliente.cpf = dados[0];
-			cliente.nome = dados[1];
-			cliente.email = dados[2];
-			cliente.telefone = dados[3];
-			clientes.add(cliente);
-			linha = leitor.readLine();
-			
-			return clientes;
-		}
-		leitor.close();
-		arquivo.close();
-		
-		throw new IllegalArgumentException("Cliente não encontrado");
-	}
-	
-	
-	
+    public Cliente(String cpf, String nome, String email, String telefone) {
+        this.cpf = cpf;
+        this.nome = nome;
+        this.email = email;
+        this.telefone = telefone;
+    }
+
+    public void salvar() throws IOException {
+        FileWriter writer = new FileWriter("clientes.csv", true);
+        writer.write("cpf,nome,email,telefone\n");
+        writer.write(this.cpf + "," + this.nome + "," + this.email + "," + this.telefone + "\n");
+        writer.close();
+    }
+
+    public void find_one(String cpf) throws IOException, ClienteNotFoundException {
+        BufferedReader reader = new BufferedReader(new FileReader("clientes.csv"));
+        String linha = null;
+        boolean found = false;
+        while ((linha = reader.readLine()) != null) {
+            String[] data = linha.split(",");
+            if (data[0].equals(cpf)) {
+                this.cpf = data[0];
+                this.nome = data[1];
+                this.email = data[2];
+                this.telefone = data[3];
+                found = true;
+                break;
+            }
+        }
+        reader.close();
+        if (!found) {
+            throw new ClienteNotFoundException("Cliente não encontrado");
+        }
+    }
+
+    public List<Cliente> find_all() throws IOException {
+        List<Cliente> clientes = new ArrayList<>();
+        BufferedReader reader = new BufferedReader(new FileReader("clientes.csv"));
+        String linha = null;
+        boolean header = true;
+        while ((linha = reader.readLine()) != null) {
+            if (header) {
+                header = false;
+                continue;
+            }
+            String[] data = linha.split(",");
+            Cliente cliente = new Cliente(data[0], data[1], data[2], data[3]);
+            clientes.add(cliente);
+        }
+        reader.close();
+        return clientes;
+    }
+    
+    public static void main(String[] args) throws IOException, ClienteNotFoundException {
+        Cliente cliente = new Cliente("12345678900", "João", "joao@email.com", "11987654321");
+        cliente.salvar();
+        Cliente clienteEncontrado = new Cliente("", "", "", "");
+        clienteEncontrado.find_one("12345678900");
+        System.out.println("Cliente encontrado: " + clienteEncontrado.nome);
+        List<Cliente> clientes = cliente.find_all();
+        System.out.println("Total de clientes: " + clientes.size());
+    }
+
+}
+
+class ClienteNotFoundException extends Exception {
+    public ClienteNotFoundException(String message) {
+        super(message);
+    }
 }
