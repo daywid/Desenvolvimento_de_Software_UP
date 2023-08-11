006e3a6e11644ec05a3084fb5fdb4f8403559a8d
Daywid <88506180+daywid@users.noreply.github.com>
2023-08-11 19:55:20 -0300

    Add files via upload

---
+++ aula_maven/src/main/java/Solid_aula2/Venda.java
@@ -0,0 +1,25 @@
+package Solid_aula2;
+
+public class Venda {
+	public double valor_a_cobrar; //atributo provisorio
+	public boolean ProcessarPagamento(CartaoDeCredito cartao) {
+		
+		boolean resultado = false;
+	
+		if(cartao.ValidarData()
+				&&cartao.ValidarNome()
+				&&cartao.ValidarNumero()) {
+			resultado = true;
+		}else {
+			resultado = false;
+		}
+		return resultado;
+	}
+	
+	
+	
+	
+	
+	
+	
+}
