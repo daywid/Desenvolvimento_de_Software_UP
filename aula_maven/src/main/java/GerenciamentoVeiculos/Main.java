6cb3d0e582a3aad867f14335ae6c7d176893208f
Daywid <88506180+daywid@users.noreply.github.com>
2023-09-24 12:31:51 -0300

    bugs fixed, ready to go

--- aula_maven/src/main/java/GerenciamentoVeiculos/Main.java
+++ aula_maven/src/main/java/GerenciamentoVeiculos/Main.java
@@ -4,9 +4,15 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        Armazenamento bancoDeDados = new BancoDeDadosArmazenamento(); // ou ArquivoArmazenamento
+        
+        // UTILIZANDO BANCO DE DADOS:
+        Armazenamento bancoDeDados = new BancoDeDadosArmazenamento(); 
         Concessionaria concessionaria = new Concessionaria(bancoDeDados);
 
+        // UTILIZANDO ARQUIVO:
+        //Armazenamento arqArmazenamento = new ArquivoArmazenamento("c://Users//daywid//Downloads//veiculos.txt");
+        //Concessionaria concessionaria = new Concessionaria(arqArmazenamento);    
+    
         Veiculo carro1 = new Carro("Ford", "Focus", 2022, 25000.0, 4);
         Veiculo moto1 = new Motocicleta("Honda", "CBR500R", 2021, 8000.0, 500);
 
